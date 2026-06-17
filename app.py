cf58392152b77bf17c313cb139c333b6ef959e09
Paul Angermeier <kontakt@paulangermeier.de>
2026-06-17 07:46:10 +0200

    Add meta to base-template and footer to index.html

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+from datetime import datetime
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    current_year = datetime.now().year
+    return render_template("index.html", current_year=current_year)
