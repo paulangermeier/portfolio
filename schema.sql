d89a5df510db374dd23c64b22d0e8f9280721e20
Paul Angermeier <kontakt@paulangermeier.de>
2026-06-27 16:00:06 +0200

    Add sections table to schema.sql and seed.py to store content for project details

--- schema.sql
+++ schema.sql
@@ -1,3 +1,6 @@
+DROP TABLE IF EXISTS sections;
+DROP TABLE IF EXISTS projects;
+
 CREATE TABLE projects (
     id INTEGER PRIMARY KEY,
     slug TEXT NOT NULL UNIQUE,
@@ -9,4 +12,15 @@ CREATE TABLE projects (
     github_url TEXT,
     image_path TEXT,
     featured INTEGER DEFAULT 0
+);
+
+CREATE TABLE sections (
+	id INTEGER PRIMARY KEY,
+	project_id INTEGER NOT NULL,
+	position INTEGER NOT NULL,
+	heading TEXT,
+	content TEXT,
+	image_path TEXT,
+	FOREIGN KEY (project_id)
+	REFERENCES projects(id)
 );
--- seed.py
+++ seed.py
@@ -34,7 +34,63 @@ projects = [
         "image_path": "images/test4.png",
         "featured": 1
     }
+]
 
+contents = [
+    {
+        "slug": "project-xy",
+        "position": 1,
+        "heading": "Test-Überschrift",
+        "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+    {
+        "slug": "project-xy",
+        "position": 2,
+        "heading": "Test-Überschrift 2",
+        "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+    {
+        "slug": "project-xy",
+        "position": 3,
+        "heading": "Test-Überschrift 3",
+        "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+    {
+        "slug": "project-z",
+        "position": 1,
+        "heading": "Überschrift-Test",
+        "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+    {
+        "slug": "project-z",
+        "position": 2,
+        "heading": "Überschrift-Test 2",
+        "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+    {
+        "slug": "project-z",
+        "position": 3,
+        "heading": "Überschrift-Test 3",
+        "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+    {
+        "slug": "project-a",
+        "position": 1,
+        "heading": "Überschrift 1",
+        "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+    {
+        "slug": "project-a",
+        "position": 2,
+        "heading": "Überschrift 2",
+        "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+    {
+        "slug": "project-a",
+        "position": 3,
+        "heading": "Überschrift 2",
+        "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
 ]
 
 # create connection to the database
@@ -44,20 +100,25 @@ con = sqlite3.connect("portfolio.db")
 cur = con.cursor()
 
 # wipe existing rows to keep this script idempotent
+cur.execute("DELETE FROM sections")
 cur.execute("DELETE FROM projects")
 
-# execute SQL Query to INSERT projects with executemany
-cur.executemany("""INSERT INTO projects (
+# create dict to store ids from DB with slug from project to correctly map contents
+slug_id = {}
+
+#execute SQL Query to INSERT projects while storing ids of projects from DB with slugs from projects
+for project in projects:
+    cur.execute("""INSERT INTO projects (
                 slug, 
-                title, 
-                year, 
-                short_description, 
-                long_description, 
-                tech_stack, 
-                github_url, 
-                image_path, 
+                title,
+                year,
+                short_description,
+                long_description,
+                tech_stack,
+                github_url,
+                image_path,
                 featured
-                ) 
+                )
                 VALUES (
                 :slug, 
                 :title, 
@@ -68,13 +129,35 @@ cur.executemany("""INSERT INTO projects (
                 :github_url,
                 :image_path,
                 :featured
-                )""", projects)
+                )""", project)
+    project_id = cur.lastrowid
+    slug_id[project["slug"]] = project_id
+
+# execute SQL Query to INSERT contents with slug_id dict
+for section in contents:
+    cur.execute("""INSERT INTO sections (
+                project_id,
+                position,
+                heading,
+                content
+                )
+                VALUES (
+                ?,
+                ?,
+                ?,
+                ?
+                )""",
+                (slug_id[section["slug"]],
+                section["position"],
+                section["heading"],
+                section["content"])
+                )
 
 # commit the transaction to save changes
 con.commit()
 
 #verify that the rows were inserted by printing rows to the terminal
-for row in cur.execute("SELECT * FROM projects ORDER BY id"):
+for row in cur.execute("SELECT * FROM projects INNER JOIN sections ON projects.id=sections.project_id ORDER BY projects.id"):
     print(row)
 
 con.close()
